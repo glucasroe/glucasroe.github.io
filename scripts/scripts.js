71390e7ab7d1efd2899b585778ce39f3eb06ed76
Lucas Roe <glucasroe@gmail.com>
2016-07-02 16:24:12 -0400

    script reconfiguring

---
+++ scripts/scripts.js
@@ -0,0 +1,36 @@
+
+smoothScroll.init({selector: '[data-scroll]',speed: 500,easing: 'easeInOutQuint',updateURL: true,offset: 16});
+//Pulled from https://github.com/cferdinandi/smooth-scroll/issues/54 Just in case
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (window.location.hash ){smoothScroll.animateScroll(  null, window.location.hash  );}
+  if(localStorage.getItem('dark-mode') == 'true'){
+    document.body.classList.add('dark-mode');
+  }
+});
+
+var trigger = document.getElementById('view-toggle');
+
+trigger.addEventListener('click', function () {
+  if (localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('dark-mode', 'false')
+    if(localStorage.getItem('inline-style') === 'true'){
+      sheet.deleteRule(0);
+      sheet.deleteRule(1);
+    }
+
+  }else{
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('dark-mode', 'true');
+  }
+});
+
+init();
+function init() {
+  localStorage.setItem('StorageTest', 'true');
+  if(localStorage.getItem('StorageTest') === 'true'){
+    trigger.classList.add('visible');
+  }
+  return;
+}
