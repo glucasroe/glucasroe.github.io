e27507ce86cea006046ae6914dfe56d77d202f0b
Luck <luck@menoetius.localdomain>
2022-01-23 13:36:46 -0500

    uncommitted old changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,27 @@
 module.exports = (eleventyConfig) => {
-	  // Copy the "assets" directory to the compiled "_site" folder.
-	  eleventyConfig.addPassthroughCopy('assets');
-	  eleventyConfig.addPassthroughCopy('images');
-	  eleventyConfig.addPassthroughCopy('style.css');
-	  eleventyConfig.addPassthroughCopy('style.css.map');
+	// Copy the "assets" directory to the compiled "_site" folder.
+	eleventyConfig.addPassthroughCopy('assets');
+	eleventyConfig.addPassthroughCopy('images');
+	eleventyConfig.addPassthroughCopy('style.css');
+	eleventyConfig.addPassthroughCopy('style.css.map');
 
-
+	eleventyConfig.addCollection('posts', collection => {
+		return collection.getFilteredByGlob('_posts/*.md');
+	});
 
 	  
-	  return {
+	return {
 		dir: {
-		  input: './',
-		  output: './_site',
-		  layouts: './_layouts',
-		},
-		templateFormats: [
-		  'html',
-		  'liquid',
-		  'md',
-		  'njk',
-		],
-		//pathPrefix: '/<repo_name>/', // omit this line if using custom domain
-	  };
+		input: './',
+		output: './_site',
+		layouts: './_layouts',
+	},
+	templateFormats: [
+		'html',
+		'liquid',
+		'md',
+		'njk',
+	],
+	//pathPrefix: '/<repo_name>/', // omit this line if using custom domain
 	};
+};
